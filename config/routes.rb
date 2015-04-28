8e3373d67a9a69516ae4d187cb532423f3ad1b82
Ming Qu <qucool@gmail.com>
2015-04-28 21:08:21 +0800

    Add devise user controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  namespace :uni do
+    devise_for :users, module: 'uni/users', path: 'account'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
