89d2ca5fde80c3ff268e617441456391c3d89bdc
Ming Qu <qucool@gmail.com>
2015-05-02 16:49:15 +0800

    Never generate view or helper specs

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,10 @@ module Campify
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+      g.view_specs false
+      g.helper_specs false
+    end
   end
 end
