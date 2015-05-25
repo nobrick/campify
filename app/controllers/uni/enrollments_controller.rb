63c59b89fb0cbbfd92458caec7ce3c9ed6acf42e
Ming Qu <qucool@gmail.com>
2015-05-25 18:52:22 +0800

    Add enrollment logic for uni users

---
+++ app/controllers/uni/enrollments_controller.rb
@@ -0,0 +1,41 @@
+class Uni::EnrollmentsController < ApplicationController
+  before_action :authenticate_uni_user!
+  before_action :set_enrollment, only: :destroy
+
+  # POST /uni/enrollments
+  def create
+    @enrollment = Enrollment.new(enrollment_params)
+    @enrollment.user = current_uni_user
+    respond_to do |format|
+      if @enrollment.save
+        format.html { redirect_to home_index_path, notice: '报名成功。' }
+      else
+        format.html { redirect_to home_index_path, alert: '报名失败。' }
+      end
+    end
+  end
+
+  # DELETE /uni/enrollments/1
+  def destroy
+    allowed = @enrollment.present?
+    @enrollment.destroy if allowed
+    respond_to do |format|
+      if allowed
+        format.html { redirect_to home_index_path, notice: '取消报名成功。' }
+      else
+        format.html { redirect_to home_index_path, alert: '取消报名失败。' }
+      end
+    end
+  end
+
+  private
+
+  def set_enrollment
+    @enrollment = Enrollment.find(params[:id])
+    @enrollment = nil if @enrollment.user != current_uni_user
+  end
+
+  def enrollment_params
+    params.require(:enrollment).permit(:showtime_id)
+  end
+end
---
+++ spec/controllers/uni/enrollments_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Uni::EnrollmentsController, type: :controller do
+  let(:user) { create :user }
+  let(:showtime) { create :showtime }
+  let(:valid_attributes) { { showtime_id: showtime.id } }
+  context 'User not signed in' do
+    describe 'Actions' do
+      it 'redirects to sign-in page' do
+        post :create, { enrollment: valid_attributes }
+        expect(response).to redirect_to new_uni_user_session_url
+      end
+    end
+  end
+
+  context 'User signed in' do
+    before { sign_in user }
+    describe 'POST #create' do
+      it 'creates showtime enrollment' do
+        expect { post :create, { enrollment: valid_attributes } }
+          .to change(Enrollment, :count).by 1
+      end
+    end
+
+    describe 'DELETE #destroy' do
+      it 'destroys showtime enrollment' do
+        valid_enrollment = create :enrollment, user_id: user.id
+        expect { delete :destroy, id: valid_enrollment.to_param }
+          .to change(Enrollment, :count).by -1
+      end
+
+      it 'only destroys enrollment that belongs to current user' do
+        invalid_enrollment = create :enrollment
+        expect { delete :destroy, id: invalid_enrollment.to_param }
+          .not_to change(Enrollment, :count)
+      end
+    end
+  end
+end
