e125a8be4c85e598a4352b27e26105ff3f5fd325
Ming Qu <qucool@gmail.com>
2015-06-09 22:51:22 +0800

    Enforce setting up redis.yml configuration

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,10 +2,13 @@ require 'connection_pool'
 
 conf_file = File.join('config','redis.yml')
 Redis::Objects.redis = ConnectionPool.new(size: 5, timeout: 5) do
-  if File.exists?(conf_file)
-    conf = YAML.load(File.read(conf_file))
-    conf[Rails.env.to_s].blank? ? Redis.new : Redis.new(conf[Rails.env.to_s])
-  else
-    Redis.new
-  end
+  # if File.exists?(conf_file)
+    # conf = YAML.load(File.read(conf_file))
+    # conf[Rails.env.to_s].blank? ? Redis.new : Redis.new(conf[Rails.env.to_s])
+  # else
+    # Redis.new
+  # end
+
+  conf = YAML.load(File.read(conf_file))
+  Redis.new(conf[Rails.env.to_s])
 end
