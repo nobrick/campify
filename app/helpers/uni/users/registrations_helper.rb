ac9c6ec9f709fc9112d9b44fc0c21d78fde180f6
Ming Qu <qucool@gmail.com>
2015-07-16 11:05:18 +0800

    Update stylesheets for themes, fonts and logos

--- app/helpers/uni/users/registrations_helper.rb
+++ app/helpers/uni/users/registrations_helper.rb
@@ -1,12 +1,4 @@
 module Uni::Users::RegistrationsHelper
-  def registration_title
-    if wechat_request? && wechat_session_set?
-      '完善资料并完成注册'
-    else
-      '注册'
-    end
-  end
-
   def tab_link_to(name, identifier, options = {})
     options.merge!({ 'aria-controls' => identifier, 'data-toggle' => 'tab', 'role' => 'tab' })
     link_to name, "##{identifier}", options
